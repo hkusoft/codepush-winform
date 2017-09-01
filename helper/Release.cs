d1357156979ef23fea4f7e52fa6ab9e084b49930
xinyustudio <xinyustudio@gmail.com>
2017-09-01 18:36:27 +0800

    Update Release class properties' DisplayName

--- helper/Release.cs
+++ helper/Release.cs
@@ -1,18 +1,45 @@
-﻿namespace codepush_winform.helper
+﻿using System.ComponentModel;
+
+namespace codepush_winform.helper
 {
     public class Release
     {
+        //[DisplayName("Test Property")]
+        //[Description("This is the description that shows up")]
+        [DisplayName("Release Binary Size")]         
         public string target_binary_range { get; set; }
+
+        [DisplayName("Release URL")]
         public string blob_url { get; set; }
+
+        [DisplayName("Description of the release")]
         public string description { get; set; }
+
+        [DisplayName("Enabled Status")]
         public bool is_disabled { get; set; }
+
+        [DisplayName("Mandatory")]
         public bool is_mandatory { get; set; }
+
+        [DisplayName("Release Label")]
         public string label { get; set; }
+
+        [DisplayName("Release Hash")]
         public string hash { get; set; }
+
+        [DisplayName("Released By")]
         public string released_by { get; set; }
+
+        [DisplayName("Release Method")]
         public string release_method { get; set; }
+
+        [DisplayName("Number of Rollout Users")]
         public int rollout { get; set; }
+
+        [DisplayName("Release Size")]
         public int size { get; set; }
+
+        [DisplayName("Update Time")]
         public long upload_time { get; set; }
 
         public override string ToString()
